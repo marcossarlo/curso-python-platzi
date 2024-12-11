9c222399f68fe678e0ad5e5f0d7d44e2f08823fb
MarcosSarLo <msl.net@gmail.com>
2024-12-10 20:09:32 -0500

    Se importó from typing import Union para hacer uso de a: Union[int, float]

--- intermedio/05-validacion-de-tipos-en-metodos.py
+++ intermedio/05-validacion-de-tipos-en-metodos.py
@@ -4,8 +4,9 @@
 Validación de Tipos en Métodos en Python:
 La validación de tipos en métodos es el proceso de verificar que los parámetros y valores retornados cumplan con los tipos esperados, asegurando que el código sea más robusto, legible y menos propenso a errores. Aunque Python es un lenguaje dinámico, donde los tipos no son estrictos en tiempo de ejecución, se pueden implementar validaciones explícitas o usar herramientas para garantizar la consistencia de los tipos.
 """
+from typing import Union
 
-def divide(a: int, b: int) -> float:
+def divide(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         raise TypeError("Los parámetros deben ser enteros o flotantes.")
     if b == 0:
@@ -13,7 +14,7 @@ def divide(a: int, b: int) -> float:
     
     return a/b
 try:
-    div_1 = divide(45,10.5)
+    div_1 = divide(45.8,12.50)
     print(f"Resultado: {div_1}")
 except (ZeroDivisionError, TypeError, Exception) as e:
     print(e)
